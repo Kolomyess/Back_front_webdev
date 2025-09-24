1d3decf2b290b424defdbff6ffdc0017723a28be
Henrique Kolomyes Silveira <henrique.kolomyes@gmail.com>
2025-09-24 08:58:04 -0300

    update clienteModel.js

--- projeto-full-ESA/backend/models/clienteModel.js
+++ projeto-full-ESA/backend/models/clienteModel.js
@@ -1,4 +1,8 @@
 //Aqui simulamos um banco de dados com uma lista de Array
 let clientes = [
-    
-]
+    {id: 1, nome: "Ana Banana", email: "banana@gmail.com", empresa: "Tech Solutions"},
+    {id: 2, nome: "João Feijão", email: "feijao@gmail.com", empresa: "Finance Corps"},
+];
+
+//Exportamos a lista para que outros arquivos possam acessá-la
+module.exports = clientes;
